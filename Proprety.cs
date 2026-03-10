7a5494711f71c3d2d237e8662c9c14077344cf6c
Fatma Mohammed Ebrahim kassem <fatmaqasem30@gmail.com>
2026-03-10 23:49:12 +0200

    Add Iszero property to Dollar class

--- Proprety.cs
+++ Proprety.cs
@@ -16,7 +16,11 @@ namespace Property
 
             dollar.Amount =9.678m; // we can set the value of the property because it has a setter
 
+          
+            Console.WriteLine(dollar.Iszero); // we can access the value of the property because it has a getter
             Console.WriteLine(dollar.Amount );
+
+
 
         }
 
@@ -45,10 +49,14 @@ namespace Property
             {
                 this._amount = processvalue(_amount);
 
+
             }
 
 
-                private decimal processvalue(decimal amount) => amount <= 0 ? 0 : Math.Round(amount, 2);
+            public bool Iszero => this ._amount == 0;// this is an expression-bodied property that returns true if the _amount field is equal to 0, and false otherwise
+
+
+            private decimal processvalue(decimal amount) => amount <= 0 ? 0 : Math.Round(amount, 2);
 
             // this is a local function that takes a decimal value and returns 0 if the value is less than or equal to 0, otherwise it returns the value itself
 
